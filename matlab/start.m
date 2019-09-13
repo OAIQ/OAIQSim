bef4c6c5a21d36f57454d8ba5de6e988ea4d27a6
nhenscheid <nph@email.arizona.edu>
2019-09-13 11:20:51 -0700

    local update

--- matlab/start.m
+++ matlab/start.m
@@ -8,9 +8,8 @@ function start(SavedConfig)
     fprintf('Welcome to OAIQSim Version %1.2f\n',Version);
 	
 	close all;
-	%addpath([pwd,'/gui'],[pwd,'/util'],[pwd,'/imaging'],[pwd,'/objects'],[pwd,'/observers'],[pwd,'/recon'])
 	addpath(genpath([pwd,'/gui']),genpath([pwd,'/imaging']),genpath([pwd,'/util']),...
-            genpath([pwd,'/objects']),genpath([pwd,'/test']),genpath([pwd,'/geom']));
+            genpath([pwd,'/objects']),genpath([pwd,'/test']),genpath([pwd,'/geom']),genpath([pwd,'/examples']));
     
     
     % Set default figure formatting
@@ -19,11 +18,4 @@ function start(SavedConfig)
     set(groot, 'defaultAxesTickLabelInterpreter','latex');  
     set(groot, 'defaultLegendInterpreter','latex');  
     
-    
-	%if(nargin>0 && isa(SavedConfig,'CGRISimGui'))
-		
-	%else
-	%	CGRISimGui;
-	%end
-
 end
